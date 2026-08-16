4258d30f2187bf3044105aac79f8272866aa5816
bsharabi <bsharabi95@gmail.com>
2026-08-16 15:37:15 +0300

    up

---
+++ Week04 - 16.08.2026/vite-test/src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import PopoverDemo from './PopoverDemo'
+
+function App() {
+
+
+  return (
+  
+      <div className='flex h-screen w-screen justify-center items-center'>
+
+        <PopoverDemo/>
+
+      </div>
+
+    
+  )
+}
+
+export default App
